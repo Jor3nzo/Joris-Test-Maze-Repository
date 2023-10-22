d668343d088c0a5e291ef50624547012c438f4db
Jor3nzo <135144670+Jor3nzo@users.noreply.github.com>
2023-10-22 16:28:22 +0300

    Spawn Point

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public GameObject spawnPoint;
     public float speed;
 
     void Update()
@@ -14,4 +15,16 @@ public class Player : MonoBehaviour
         var finalPosition = Vector3.MoveTowards(transform.position, mousePos, speed * Time.deltaTime);
         GetComponent<Rigidbody2D>().MovePosition(finalPosition);
     }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+
+
+        if (collision.gameObject.name.Contains("Wall"))
+        {
+            transform.position = spawnPoint.transform.position;
+
+        }
+    }
+
 }
