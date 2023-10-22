fc5bb299925596d942e882859d30a711bf966b94
Jor3nzo <135144670+Jor3nzo@users.noreply.github.com>
2023-10-22 16:58:27 +0300

    Jumpscare

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,8 +6,17 @@ public class Player : MonoBehaviour
 {
     public GameObject spawnPoint;
     public float speed;
+    public GameObject jumpscareImage;
+    public AudioSource jumpscareSound;
 
-    void Update()
+
+    void Start()
+    {
+        Cursor.visible = false;
+        jumpscareSound = GetComponent<AudioSource>();
+    }
+
+        void Update()
     {
         var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePos.z = 0;
@@ -27,4 +36,15 @@ public class Player : MonoBehaviour
         }
     }
 
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Contains("Jumpscare"))
+        {
+            jumpscareImage.SetActive(true);
+            Time.timeScale = 0;
+            jumpscareSound.Play();
+        }
+    }
 }
+
+
